d05c43476d1dda99caccb96e590204dd4b816503
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-22 16:32:02 -0300

    Organização dos componentes: A primeeira aula foi uma grande introdução ao React, com a elaboração e concatenação dos componenste. Levando em consideração que o React usa JS pra criação de HTML(Por baixo dos panos) ele é de fato mais dinamico por ser uma base de 'reação' de diferentes componentes criado como Funções JS

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return(  
+        <header className='banner'>     
+            <img src="/img/banner.png" alt="O banner do header"/>
+        </header>
+        
+    )
+}
+
+export default Banner
