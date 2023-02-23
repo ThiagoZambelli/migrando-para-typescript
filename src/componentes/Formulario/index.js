eafa1c5c6bedfbe46475e4563f2f94ee8f9561fe
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-23 18:16:03 -0300

    utilização do useState para criação de um objeto com toods os valores do formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,7 +3,7 @@ import Botao from "../Botao"
 import CampoTexto from "../CampoTexto/CampoTexto"
 import ListaSuspenca from "../ListaSuspenca"
 import "./Formulario.css"
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-End',
@@ -21,7 +21,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento)=>{
         evento.preventDefault();
-        console.log("Funcionou", nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
