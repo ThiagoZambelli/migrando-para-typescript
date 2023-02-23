a3d93357fa8ba8149fe73f29869a2927f4135b11
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-23 18:49:04 -0300

    Criação do componente Time e elaboração de um mapa do que sera necessario nele

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 const [colaboradores, setColaboradores] = useState([]);
 
 const aoNovoColaborador = (colaborador)=>{
   setColaboradores([...colaboradores, colaborador])
+  console.log(colaboradores)
 } 
 
   return (
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaborador(colaborador)}/>
+      <Time nome="Programação"/>
     </div>
   );
 }
